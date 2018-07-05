4bbb0b3c5ffe76af8d2162322e50863d2cecc6c8
genz84 <greg.genz@free.fr>
2018-07-05 18:27:11 +0200

    First commit

---
+++ gatsby-config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  plugins:
+  [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
+    `gatsby-plugin-glamor`,
+    `gatsby-transformer-remark`,
+    `gatsby-plugin-netlify-cms`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+  ],
+  siteMetadata: {
+   title: `Pandas Eating Lots`,
+ },
+};
